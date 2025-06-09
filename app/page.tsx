a369a20b54b1845aa399a59d54f07b31c4b2d509
chrollos <anon>
2025-06-09 11:43:46 +0800

    commit init file

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,16 @@ import React from "react";
 
 export default function Home () {
   return (
-    <div>
-      <h1>Hello World</h1>
+
+  <div className="Body border-2 border-amber-50 border-solid p-7 m-20">
+    <div className="Button backdrop-blur-2xl bg-green-400 text-2xl">
+      <a href="layout.tsx">Button 1</a>
     </div>
+    <div className="Hello-Button m-7 text-green-600 shadow-2xl inset-shadow-2xl hover:text-2xl hover:text-amber-600 hover:animate-bounce scroll-smooth">
+      <p>Hello Neil</p>
+    </div>
+
+  </div>
+  
   )
 }
